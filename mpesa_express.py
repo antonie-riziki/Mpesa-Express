5fe98c5fcbdb252e1a068d7eb05372d6475fd893
antonie <53366054+antonie-riziki@users.noreply.github.com>
2022-02-17 21:54:40 +0300

    Add files via upload

---
+++ mpesa_express.py
@@ -0,0 +1,39 @@
+import streamlit as st 
+
+header = st.container()
+body1 = st.container()
+body2 = st.container()
+
+
+with header:
+	st.title('Lipa na Mpesa')
+	st.subheader('Mpesa Express API')
+	st.text('Merchant initiated online payment')
+
+with body1:
+	with st.form(key='stk_push'):
+		st.markdown('Mpesa STK Push')
+		amount = st.text_input(label='amount')
+		phone = st.text_input(label='phone number')
+		submit = st.form_submit_button(label='proceed payment')
+
+
+with body2:
+	col1, col2 = st.columns(2)
+
+	with col1:
+		with st.form(key='buygoodsonline'):
+			st.markdown('Buy Goods Online')
+			st.write('Till number	234189')
+			amount = st.text_input(label='amount')
+			phone = st.text_input(label='phone number')
+			submit = st.form_submit_button(label='proceed payment')
+
+	with col2:
+		with st.form(key='paybillonline'):
+			st.markdown('Paybill Online')
+			st.write('Business no. 	206206')
+			amount = st.text_input(label='amount')
+			phone = st.text_input(label='phone number')
+			submit = st.form_submit_button(label='proceed payment')		
+
